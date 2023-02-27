d05d0b55f4ceb36dbe6e289dfe109f5608704022
harrydennis <harrydnnis@gmail.com>
2023-02-27 13:48:06 -0800

    1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,17 +1,14 @@
-/*
- * File: 1-swap.c
- * Auth: harry dennis
- */
-
 #include "main.h"
 /**
- * swap_int - swaps the value of two integers
+ * swap_int - swaps the values of two integers
  * @a: integer to swap
- * @b:integer to swap
+ * @b: integer to swap
  */
-void swap_int(int *a, int *b);
+void swap_int(int *a, int *b)
 {
-	int tmp = *a;
+	int m;
+
+	m = *a;
 	*a = *b;
-	*b = tmp;
+	*b = m;
 }
