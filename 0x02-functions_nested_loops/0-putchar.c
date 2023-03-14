682288bdd5ce71a1c3152f705fe2a2db1dba4d11
harrydennis <harrydnnis@gmail.com>
2023-03-14 05:14:10 -0700

    update

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _putchar - prints alphabets
  * main - print _putchar
--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -6,6 +6,7 @@
  * @a: the matrix of integers
  * @size: the size of the matrix
  */
+
 void print_diagsums(int *a, int size)
 {
 	int index, sum1 = 0, sum2 = 0;
@@ -16,11 +17,13 @@ void print_diagsums(int *a, int size)
 		a += size;
 	}
 
-	a -= size;
+		a -= size;
+
 	 for (index = 0; index < size; index++)
 	{
 		sum2 += a[index];
 		a -= size;
 	}
-	 printf("%d, %d\n", sum1, sum2);
+
+	 printf("%d, %d\n", sum1,sum2);
 }
